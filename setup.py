a9bee6abfe9c68bad9e977e3ec6cf605d29199af
Sean Tempesta <sean.tempesta@gmail.com>
2020-10-15 11:08:29 +0800

    glow-tts requires this package and it's easier to install separately

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    name='monotonic_align',
+    ext_modules=cythonize("core.pyx"),
+    include_dirs=[numpy.get_include()],
+    install_requires=[
+        'Cython',
+        'numpy'
+    ]
+)
