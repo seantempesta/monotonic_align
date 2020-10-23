eded5e30dab1b36625093d55b1b29eeef356ac72
Sean Tempesta <sean.tempesta@gmail.com>
2020-10-23 14:32:08 +0800

    This works from glow-tts now using monotonic.maximum_path

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
 import numpy
 
 setup(
     name='monotonic_align',
+    packages=find_packages(),
     ext_modules=cythonize("core.pyx"),
     include_dirs=[numpy.get_include()],
     install_requires=[
         'Cython',
-        'numpy'
+        'numpy',
+        'torch'
     ]
 )
